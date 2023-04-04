661f966dca20e9ffff5bf46c6eb5d337468e2a86
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-04 06:03:37 +0100

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,5 +1,6 @@
-terraform {
-  backend "s3" {
+data "terraform_remote_state" "networking" {
+  backend = "s3"
+  config = {
     bucket = "khatrig-githubaction"
     key = "layer-new.tfstate"
     region = "us-east-1"
